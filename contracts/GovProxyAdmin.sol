3d7a61c5d5e02ff8873fdf4f23521f4fcfd8ec1f
Tobeyw <tobey1024@126.com>
2023-09-15 18:36:08 +0800

    update

--- contracts/GovProxyAdmin.sol
+++ contracts/GovProxyAdmin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "./GovernanceVote.sol";
 
 /**
